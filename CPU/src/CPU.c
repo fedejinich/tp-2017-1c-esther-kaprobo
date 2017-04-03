22e61b9256eab022f8778d8a39bfa9d7528aadd5
Rodrigo Alvarez <ro.frba@gmail.com>
2017-04-02 23:42:09 -0300

    creacion de todos los demas procesos, cada uno en su carpeta

---
+++ CPU/src/CPU.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : CPU.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Creacion proceso CPU");
+	return EXIT_SUCCESS;
+}
---
+++ File_System/src/File_System.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : File_System.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Creacion Proceso File System");
+	return EXIT_SUCCESS;
+}
