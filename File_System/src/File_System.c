848e112f3a5564029df35bd7606858da8f5dc313
FedeJinich <jinichfede@gmail.com>
2017-04-06 00:40:29 -0300

    Cargo y leo file_system.config

--- File_System/src/File_System.c
+++ File_System/src/File_System.c
@@ -10,8 +10,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "File_System.h"
 
-int main(void) {
-	puts("Creacion Proceso File System");
+int main(int argc, char **argv) {
+
+	cargarConfiguracion();
+
 	return EXIT_SUCCESS;
+}
+
+
+void cargarConfiguracion() {
+
+	t_config* config = config_create("file_system.config");
+	puerto = config_get_string_value(config, "PUERTO");
+	printf("Puerto: %s \n",puerto);
+	puntoMontaje = config_get_string_value(config, "PUNTO_MONTAJE");
+	printf("Punto Montaje: %s \n",puntoMontaje);
+
 }
---
+++ File_System/src/File_System.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/config.h>
+
+
+int puerto;
+char* puntoMontaje;
+
+void cargarConfiguracion();
