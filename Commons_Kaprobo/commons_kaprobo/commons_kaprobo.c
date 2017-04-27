d72b4ab79c4f0f409abd5ed33175377a5bd8c5c4
FedeJinich <jinichfede@gmail.com>
2017-04-26 22:13:33 -0300

    Agrego commons nuestras

---
+++ Commons_Kaprobo/commons_kaprobo/commons_kaprobo.c
@@ -0,0 +1,1 @@
+#include "commons_kaprobo.h"
---
+++ Commons_Kaprobo/commons_kaprobo/commons_kaprobo.h
@@ -0,0 +1,24 @@
+#ifndef COMMONS_KAPROBO_COMMONS_KAPROBO_H_
+#define COMMONS_KAPROBO_COMMONS_KAPROBO_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdint.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <string.h>
+#include <commons/string.h>
+#include <commons/error.h>
+#include <commons/log.h>
+
+
+void iniciarLog(char* nombreDelArchivoLog);
+
+
+
+
+#endif /* COMMONS_KAPROBO_H_ */
