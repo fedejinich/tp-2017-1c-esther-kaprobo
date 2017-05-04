f58808a4ada580c11a3d40e3e402ca934e7b0645
FedeJinich <jinichfede@gmail.com>
2017-05-04 01:18:30 -0300

    Creo una carpeta con sus respectivos .c y .h para manejar los diferentes hilos de Memoria y en pos de mejorar la cohesion de Memoria.c (we re nerd era el chabon)

---
+++ Memoria/src/hilos/hiloKernel.c
@@ -0,0 +1,66 @@
+/*
+ * hiloKernel.c
+ *
+ *  Created on: 1/5/2017
+ *      Author: utnso
+ */
+
+#include "hiloKernel.h"
+
+void* hiloServidorKernel(void* arg) {
+	log_info(logger,"------Hilo Kernel------\n");
+	int servidorSocket, socketCliente;
+	int *socketClienteTemp;
+	socketKernel = socket_escucha("127.0.0.1", puerto);
+	log_info(logger,"Creacion socket servidor Kernel exitosa\n");
+	listen(socketKernel, 1024);
+	while(1) {
+		socketCliente = aceptar_conexion(socketKernel);
+		log_info(logger,"Iniciando Handshake con Kernel\n");
+		bool resultado_hand = esperar_handshake(socketCliente,13);
+		if(resultado_hand){
+			log_info(logger,"Conexión aceptada del Kernel %d!!\n", socketCliente);
+		} else {
+			log_info(logger,"Handshake fallo, se aborta conexion\n");
+			exit (EXIT_FAILURE);
+		}
+		socketClienteTemp = malloc(sizeof(int));
+		*socketClienteTemp = socketCliente;
+		pthread_t conexionKernel;
+		pthread_create(&conexionKernel, NULL, hiloConexionKernel, (void*)socketClienteTemp);
+	}
+}
+
+
+void* hiloConexionKernel(void* socketKernel) {
+
+
+	t_paquete * paquete_nuevo;
+	int pid, paginasRequeridas, tamanioCodigo;
+
+	while (1) {
+		paquete_nuevo = recibir(socketKernel);
+		switch (paquete_nuevo->codigo_operacion) {
+			case 1: //1 en realidad  deberia ser INICIAR_PROCESO
+				memcpy(&pid, paquete_nuevo->data, sizeof(int));
+				memcpy(&paginasRequeridas, paquete_nuevo->data + sizeof(int),sizeof(int));
+
+				log_info(logger,"Llega un nuevo proceso, las paginas requeridas son %d.\n",paginasRequeridas);
+
+				if (espacioDisponible(pid, paginasRequeridas)) {
+					alojarEnMemoria(pid, paginasRequeridas);
+					log_info(logger,"Se pudo inicializar el proceso con el pid %d.\n",pid);
+					enviar(socketKernel, 2, sizeof(int), &pid); //2 DEBERIA SER EXITO
+				} else {
+					log_info(logger,"No se pudo inicializar el proceso con el pid %d.\n",pid);
+					enviar(socketKernel, 3, sizeof(int), &pid);//3 DEBERIA SER FRACASO
+				}
+			break;
+			default:
+				break;
+		}
+
+	}
+
+	return 1;
+}
