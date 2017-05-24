ff5d1345cc8d1d81494f1255d043c39c97ba3ae8
Fede Jinich <jinichfede@gmail.com>
2017-05-23 22:59:01 -0300

    Creo funciones para manejar la tabla de pagina dentro de memoria

--- Memoria/src/funcionesAuxiliares/funcionesAuxiliares.c
+++ Memoria/src/funcionesAuxiliares/funcionesAuxiliares.c
@@ -18,8 +18,30 @@ int getCantidadDeFramesTablaDePaginas() {
 	return parteEntera;
 }
 
-
+int getCantidadDeEntradasPorFrame() {
+	return getParteEntera(frame_size/sizeof(t_entradaTablaDePaginas));
+}
 
 int getLimiteFrameByOffset(int offset) {
 	return getParteEntera(frame_size/offset) * offset;
+}
+
+int numeroDeEntradaEnFrameBy(int numeroDeEntradaTablaDePaginas) {
+	if(numeroDeEntradaTablaDePaginas == 0) //Pattern matching
+		return 0;
+	int cantidadDeEntradasPorFrame = getCantidadDeEntradasPorFrame();
+	int numeroDeEntrada = numeroDeEntradaTablaDePaginas % cantidadDeEntradasPorFrame;
+	if(numeroDeEntrada == 0) //Mas pattern matching en el caso de que la entrada sea multiplo de 21
+		return 21;
+
+	return numeroDeEntrada;
+}
+
+int numeroDeFrameBy(int numeroDeEntradaTablaDePaginas) {
+	if(numeroDeEntradaTablaDePaginas == 0) //Pattern matching
+		return 0;
+	int numeroDeFrame = getParteEntera(numeroDeEntradaTablaDePaginas/getCantidadDeEntradasPorFrame());
+	if((numeroDeEntradaTablaDePaginas % getCantidadDeEntradasPorFrame()) == 0) //Mas pattern matching
+		return numeroDeFrame - 1;
+	return numeroDeFrame;
 }
--- Memoria/src/funcionesAuxiliares/funcionesAuxiliares.h
+++ Memoria/src/funcionesAuxiliares/funcionesAuxiliares.h
@@ -10,7 +10,29 @@
 
 #include "../Memoria.h"
 
+/**
+* @NAME: getCantidadDeEntradasPorFrame
+* @DESC: Retorna la cantidad de entradas de tabla de pagina que puede contener un frame (EJ. frame_size = 256bytes -> 20 entradas)
+*/
 int getCantidadDeFramesTablaDePaginas();
+
+/**
+* @NAME: getLimiteFrameByOffset
+* @DESC: En base a un offset me dice hasta que posicion de memoria
+* puedo escribir en un frame (EJ frame_size = 256, offset = 12 -> limite = 252)
+*/
 int getLimiteFrameByOffset(int offset);
+
+/**
+* @NAME: numeroDeFrameBy
+* @DESC: Retorna el numero frame en el que esta la entrada a la tabla de paginas
+*/
+int numeroDeFrameBy(int numeroDeEntradaTablaDePaginas);
+
+/**
+* @NAME: numeroDeEntradaEnFrameBy
+* @DESC: Retorna el numero de entrada dentro de un frame (EJ: EntradaReal = 21 -> Entrada = 1 del Frame = 1)
+*/
+int numeroDeEntradaEnFrameBy(int numeroDeEntradaTablaDePaginas);
 
 #endif /* FUNCIONESAUXILIARES_FUNCIONESAUXILIARES_H_ */
