6958d444b4a5314a43ee60486381f040b7f8c422
Fede Jinich <jinichfede@gmail.com>
2017-06-14 23:12:55 -0300

    asd

--- Memoria/src/hilos/hiloKernel.h
+++ Memoria/src/hilos/hiloKernel.h
@@ -14,18 +14,7 @@ void* hiloServidorKernel(void* arg);
 void* hiloConexionKernel(void* socketKernel);
 
 
-enum {
-	PEDIDO_DE_PAGINAS,
-	PEDIDO_DE_PAGINAS_OK,
-	PEDIDO_DE_PAGINAS_FALLO
-} mensajesKernelMemoria; //HAY QUE FIXEAR ESTO
 
-enum {
-	INICIALIZAR_PROCESO,
-	ASIGNAR_PAGINAS,
-	SOLICITAR_BYTES,
-	ALMACENAR_BYTES,
-	FINALIZAR_PROCESO
-} operaciones;
+
 
 #endif /* HILOS_HILOKERNEL_H_ */
