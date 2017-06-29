17672d6ad4d135519f8a3e456f7945f11f5cc5b5
FedeJinich <jinichfede@gmail.com>
2017-06-29 12:20:15 -0300

    Solicitar bytes agrega a cache PID si lee de memoria

--- Memoria/src/operaciones/operacionesMemoria-CPU.c
+++ Memoria/src/operaciones/operacionesMemoria-CPU.c
@@ -11,20 +11,24 @@
 void* solicitarBytesDePagina(int pid, int pagina, int offset, int tamanio) {
 	//PEDIDO DE LECTURA POR PARTE DE CPU
 	//RETORNA NULL SI NO ENCUNETRA NADA
-	retardo();
+	retardo(); //EL RETARDO VA ACA O VA CUANDO EMPIEZA EL ELSE? LA CACHE SE AFECTA POR EL RETARDO?
 	void* buffer = -1;
 	log_info(logger, "Solicitando bytes de PID: %i, pagina: %i, offset: %i y tamanio: %i", pid, pagina, offset, tamanio);
 
 	if(estaEnCache(pid, pagina))
 		leerDeCache(pid, pagina);
 	else {
+		retardo();
 		t_entradaTablaDePaginas* entrada = getEntradaTablaDePaginasHash(pid, pagina);
+
 		if(entrada == EXIT_FAILURE) {
 			log_error(logger, "Error en solicitar bytes de una pagina");
 			return EXIT_FAILURE;
 		}
+
 		log_info(logger, "Los bytes del PID: %i, pagina: %i se encuentran en el frame %i", pid, pagina, entrada->frame);
 		void* buffer = leerFrame(entrada->frame, offset, tamanio);
+		escribirCache(pid, pagina, buffer);
 	}
 
 	return buffer;
